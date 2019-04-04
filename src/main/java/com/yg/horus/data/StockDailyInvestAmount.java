9fad18288addd60b97c9775e9110c2ec6db72c36
jeff1865 <jeff.yg.kim@gmail.com>
2019-04-04 18:36:24 +0900

    daily commit

---
+++ src/main/java/com/yg/horus/data/StockDailyInvestAmount.java
@@ -0,0 +1,18 @@
+package com.yg.horus.data;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by 1002000 on 2019. 4. 4..
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class StockDailyInvestAmount {
+    ;
+
+}
